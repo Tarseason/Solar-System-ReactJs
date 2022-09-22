c18d5c9de57fbe2d663e4a48b8bdc50dd7a06c3e
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-09-22 17:37:52 -0400

    Conclui requisito 9

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import Misssions from '../data/missions';
+import MissionsCard from './MissionsCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {Misssions.map((x) => (<MissionsCard
+          key={ x.name }
+          name={ x.name }
+          year={ x.year }
+          country={ x.country }
+          destination={ x.destination }
+        />))}
       </div>
     );
   }
